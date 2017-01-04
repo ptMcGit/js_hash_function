3a1f4e6645c6de86da9b02c9b58abde8e82b36c1
Zach H <zdhadgraft@gtcc.edu>
2017-01-04 12:08:13 -0500

    Finished myHash object.

--- trello_exercise.js
+++ trello_exercise.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var letters = "acdegilmnoprstuw";
-
 var num = 37;
 
 function createHash(init, num, chars){
@@ -16,38 +15,23 @@ function createHash(init, num, chars){
                 h = (h * num + chars.indexOf(s[i]));
             return h
         }
+        this.reverseHash = function(n){
+            var s ='';
+            while(n > init){
+                for(i = 0; i < chars.length; i++) {
+                    if((n - i) % num === 0) {
+                n = ((n - i) / num);
+                if(n > 0)
+                    s += chars[i];
+                    }
+                }
+            }
+            return s.split("").reverse().join("");
+        }
     }
     return new MyHash(init);
 }
 
-function myHash64(s){
-    var h = 7;
-    var i;
-    for(i = 0; i < s.length; i++)
-        h = (h * num + letters.indexOf(s[i]));
-    return h;
-}
-console.log(myHash64("leepadg"));
-
-function reverseMyHash64(n){
-    var s ='';
-    while(n > 7){
-        for(i = 0; i < letters.length; i++) {
-            if((n - i) % num === 0) {
-                n = ((n - i) / num);
-                if(n > 0)
-                    s += letters[i];
-
-            }
-        }
-    }
-    return s.split("").reverse().join("");
-}
-
-console.log(reverseMyHash64(930846109532517));
-
-console.log(myHash64("leepadg"));
-console.log(reverseMyHash64(680131659347));
-
 var x = createHash(7,37,'acdegilmnoprstuw');
 console.log(x.hash("leepadg"));
+console.log(x.reverseHash(930846109532517));
